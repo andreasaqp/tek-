c074c9153627f8125dcb36fc6b5053b786b72014
Mlinnelyst <mlinnelyst11@gmail.com>
2020-11-15 17:59:48 +0100

    a

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const { exec } = require('child_process');
+const express = require('express');
 
-exec('cvlc alarm.mp3');
+const app = express();
+
+function playSound() {
+	exec('cvlc alarm.mp3');
+}
+
+app.get('/play', (req, res) => {
+	playSound();
+	res.send('Playing sound.');
+});
+
+app.listen(3000, () => {
+	console.log('Server running on port 3000');
+});
