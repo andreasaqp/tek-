3a0983f65fed7e8a61e85d0ccc3faabaf2b75499
Mlinnelyst <mlinnelyst11@gmail.com>
2020-11-15 18:28:22 +0100

    a

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const { exec } = require('child_process');
 const express = require('express');
-
+const { exec } = require('child_process');
 const app = express();
+const Gtts = require('gtts');
 
-function playSound() {
-	exec('cvlc alarm.mp3');
-}
+app.get('/:text', function (req, res) {
+	const gtts = new Gtts(req.params.text, 'en');
 
-app.get('/play', (req, res) => {
-	playSound();
-	res.send('Playing sound.');
+	gtts.save('warning.mp3', function (err, result) {
+		console.log(`Saved warning=${req.params.text} to mp3!`);
+
+		exec('cvlc warning.mp3');
+	});
+
+	res.send('Playing warning.');
 });
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
+app.listen(3000, function () {
+	console.log('Program started!');
 });
